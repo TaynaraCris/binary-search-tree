672421131189787c227892232378fab2c9744139
Vinicius Vieira <64455111+viniciusvieira00@users.noreply.github.com>
2021-10-31 22:20:59 -0300

    Update arvore.cpp

--- arvore.cpp
+++ arvore.cpp
@@ -2,103 +2,154 @@
 
 using namespace std;
 
-string replace(string values_string){
- 
-     for(int i =  0; i < values_string.length(); i++){
-         if(values_string[i] == ','){
-            values_string[i] = ' ';
-         }
-     }
-     return values_string;
- }
+string replace(string values_string)
+{
 
-struct No {
-    int Chave;
-    struct No *pEsq{}, *pDir{};
-    int FatBal;
+	for (int i = 0; i < values_string.length(); i++)
+	{
+		if (values_string[i] == ',')
+		{
+			values_string[i] = ' ';
+		}
+	}
+	return values_string;
+}
+
+struct No
+{
+	int Chave;
+	struct No *pEsq{}, *pDir{};
+	int FatBal;
 };
 
-void insere_node(int value, No *&root){
-    if (root == nullptr) { /*inserir x como raiz da árvore*/
-    	root = (No*)malloc(sizeof(No));
-    	root->Chave = value;
-    	root->pEsq = nullptr;
-    	root->pDir = nullptr;
-    } else {
-    	if (value < root->Chave) {
-    		insere_node(value, root->pEsq);
-    	}
-    	if (value > root->Chave) {
-    		insere_node(value, root->pDir);
-    	}
+void insere_node(int value, No *&root)
+{
+	if (root == nullptr)
+	{ /*inserir x como raiz da árvore*/
+		root = (No *)malloc(sizeof(No));
+		root->Chave = value;
+		root->pEsq = nullptr;
+		root->pDir = nullptr;
+	}
+	else
+	{
+		if (value < root->Chave)
+		{
+			insere_node(value, root->pEsq);
+		}
+		if (value > root->Chave)
+		{
+			insere_node(value, root->pDir);
+		}
 	}
 };
 
-void inorder(struct No* root)
+struct No *liberaNo(struct No *root, int Chave)
 {
-    if (root != NULL) {
-        inorder(root->pEsq);
-        printf("%d  %d\n",root->Chave, root->FatBal);
-        inorder(root->pDir);
-    }
+	if (root == NULL)
+		return root;
+	if (Chave < root->Chave)
+		root->pEsq = liberaNo(root->pEsq, Chave);
+	else if (Chave > root->Chave)
+		root->pDir = liberaNo(root->pDir, Chave);
+	else
+	{
+		if (root->pEsq == NULL and root->pDir == NULL)
+			return NULL;
+		else if (root->pEsq == NULL)
+		{
+			struct No *temp = root->pDir;
+			free(root);
+			return temp;
+		}
+		else if (root->pDir == NULL)
+		{
+			struct No *temp = root->pEsq;
+			free(root);
+			return temp;
+		}
+		return root;
+	}
 }
 
-void printa(No * root){
-    if (root != nullptr) { /*inserir x como raiz da árvore*/
+void inorder(struct No *root)
+{
+	if (root != NULL)
+	{
+		inorder(root->pEsq);
+		printf("%d  %d\n", root->Chave, root->FatBal);
+		inorder(root->pDir);
+	}
+}
+
+void printa(No *root)
+{
+	if (root != nullptr)
+	{ /*inserir x como raiz da árvore*/
 		cout << root->Chave << endl;
 		printa(root->pEsq);
 		printa(root->pDir);
-    }
+	}
 };
 
-int getAltura(No* Node)
+int getAltura(No *No)
 {
-    if (Node == nullptr)
-        return 0;
-    else {
-        /* computa a altura de cada subArvore */
-        int lHeight = getAltura(Node->pEsq);
-        int rHeight = getAltura(Node->pDir);
+	if (No == nullptr)
+		return 0;
+	else
+	{
+		/* computa a altura de cada subArvore */
+		int lHeight = getAltura(No->pEsq);
+		int rHeight = getAltura(No->pDir);
 
-        /* usa a maior altura */
-        if (lHeight > rHeight){
-            return (lHeight + 1);
+		/* usa a maior altura */
+		if (lHeight > rHeight)
+		{
+			return (lHeight + 1);
 		}
-        else{
-            return (rHeight + 1);
+		else
+		{
+			return (rHeight + 1);
 		}
-    }
+	}
 }
 
-void calculaFatBal(No * root){
-	if(root != nullptr){
+void calculaFatBal(No *root)
+{
+	if (root != nullptr)
+	{
 		calculaFatBal(root->pEsq);
-		calculaFatBal(root ->pDir);
+		calculaFatBal(root->pDir);
 
 		root->FatBal = (getAltura(root->pEsq) - getAltura(root->pDir));
 	}
 }
 
-int main(){
+int main()
+{
 
-    fstream file;
-    file.open("arquivo.csv", ios::in);
+	fstream file;
+	file.open("arquivo.csv", ios::in);
 
-    string values_string;
+	string values_string;
 
-    int values[10000];
-    int ind = 0;
-    string number = "";
+	int values[10000];
+	int ind = 0;
+	string number = "";
 
-	while(file.good()){
+	while (file.good())
+	{
 		getline(file, values_string);
 		values_string = replace(values_string);
 
-		for(int i = 0; i < values_string.size(); ++i){
-			if(values_string[i] >= '0' && values_string[i] <= '9'){
+		for (int i = 0; i < values_string.size(); ++i)
+		{
+			if (values_string[i] >= '0' && values_string[i] <= '9')
+			{
 				number += values_string[i];
 			}
-			else if((values_string[i] == ' ' && number != "") || values_string[i] == '\0'){
+			else if ((values_string[i] == ' ' && number != "") || values_string[i] == '\0')
+			{
 				values[ind] = stoi(number);
 				ind++;
 				number = "";
@@ -108,21 +159,23 @@ int main(){
 		values[ind] = stoi(number);
 	}
 
-    No * root = nullptr;
-
-	
+	No *root = nullptr;
 
 	// Gera a arvore
-    for(int i = 0; i <= ind;++i) {
+	for (int i = 0; i <= ind; ++i)
+	{
 		insere_node(values[i], root);
-    }
+	}
 
 	calculaFatBal(root);
 
 	cout << "Printando em ordem crescente:" << endl;
 	// Printando os nos
-	//printa(root);	
+	//printa(root);
 	inorder(root);
 
-    return 0;
+	//Inserir a chave abaixo e descomentar para liberar os nós
+	//root = liberaNo(root, Chave);
+
+	return 0;
 }
