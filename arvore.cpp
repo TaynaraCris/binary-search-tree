12558f828e0f40acd14b0e79430967b3f0b7da6e
GianMedeiros <gianmedeiros14@gmail.com>
2021-11-02 13:18:35 -0300

    :construction: passando as funções para uma biblioteca.

--- arvore.cpp
+++ arvore.cpp
@@ -1,129 +1,8 @@
 #include <bits/stdc++.h>
+#include "edalib.h"
 
 using namespace std;
 
-string replace(string values_string)
-{
-
-	for (int i = 0; i < values_string.length(); i++)
-	{
-		if (values_string[i] == ',')
-		{
-			values_string[i] = ' ';
-		}
-	}
-	return values_string;
-}
-
-struct No
-{
-	int Chave;
-	struct No *pEsq{}, *pDir{};
-	int FatBal;
-};
-
-void insere_node(int value, No *&root)
-{
-	if (root == nullptr)
-	{ /*inserir x como raiz da árvore*/
-		root = (No *)malloc(sizeof(No));
-		root->Chave = value;
-		root->pEsq = nullptr;
-		root->pDir = nullptr;
-	}
-	else
-	{
-		if (value < root->Chave)
-		{
-			insere_node(value, root->pEsq);
-		}
-		if (value > root->Chave)
-		{
-			insere_node(value, root->pDir);
-		}
-	}
-};
-
-struct No *liberaNo(struct No *root, int Chave)
-{
-	if (root == NULL)
-		return root;
-	if (Chave < root->Chave)
-		root->pEsq = liberaNo(root->pEsq, Chave);
-	else if (Chave > root->Chave)
-		root->pDir = liberaNo(root->pDir, Chave);
-	else
-	{
-		if (root->pEsq == NULL and root->pDir == NULL)
-			return NULL;
-		else if (root->pEsq == NULL)
-		{
-			struct No *temp = root->pDir;
-			free(root);
-			return temp;
-		}
-		else if (root->pDir == NULL)
-		{
-			struct No *temp = root->pEsq;
-			free(root);
-			return temp;
-		}
-		return root;
-	}
-}
-
-void inorder(struct No *root)
-{
-	if (root != NULL)
-	{
-		inorder(root->pEsq);
-		printf("%d  %d\n", root->Chave, root->FatBal);
-		inorder(root->pDir);
-	}
-}
-
-void printa(No *root)
-{
-	if (root != nullptr)
-	{ /*inserir x como raiz da árvore*/
-		cout << root->Chave << endl;
-		printa(root->pEsq);
-		printa(root->pDir);
-	}
-};
-
-int getAltura(No *No)
-{
-	if (No == nullptr)
-		return 0;
-	else
-	{
-		/* computa a altura de cada subArvore */
-		int lHeight = getAltura(No->pEsq);
-		int rHeight = getAltura(No->pDir);
-
-		/* usa a maior altura */
-		if (lHeight > rHeight)
-		{
-			return (lHeight + 1);
-		}
-		else
-		{
-			return (rHeight + 1);
-		}
-	}
-}
-
-void calculaFatBal(No *root)
-{
-	if (root != nullptr)
-	{
-		calculaFatBal(root->pEsq);
-		calculaFatBal(root->pDir);
-
-		root->FatBal = (getAltura(root->pEsq) - getAltura(root->pDir));
-	}
-}
 
 int main()
 {
@@ -179,3 +58,128 @@ int main()
 
 	return 0;
 }
+
+
+// string replace(string values_string)
+// {
+
+// 	for (int i = 0; i < values_string.length(); i++)
+// 	{
+// 		if (values_string[i] == ',')
+// 		{
+// 			values_string[i] = ' ';
+// 		}
+// 	}
+// 	return values_string;
+// }
+
+// struct No
+// {
+// 	int Chave;
+// 	struct No *pEsq{}, *pDir{};
+// 	int FatBal;
+// };
+
+// void insere_node(int value, No *&root)
+// {
+// 	if (root == nullptr)
+// 	{ /*inserir x como raiz da árvore*/
+// 		root = (No *)malloc(sizeof(No));
+// 		root->Chave = value;
+// 		root->pEsq = nullptr;
+// 		root->pDir = nullptr;
+// 	}
+// 	else
+// 	{
+// 		if (value < root->Chave)
+// 		{
+// 			insere_node(value, root->pEsq);
+// 		}
+// 		if (value > root->Chave)
+// 		{
+// 			insere_node(value, root->pDir);
+// 		}
+// 	}
+// };
+
+// struct No *liberaNo(struct No *root, int Chave)
+// {
+// 	if (root == NULL)
+// 		return root;
+// 	if (Chave < root->Chave)
+// 		root->pEsq = liberaNo(root->pEsq, Chave);
+// 	else if (Chave > root->Chave)
+// 		root->pDir = liberaNo(root->pDir, Chave);
+// 	else
+// 	{
+// 		if (root->pEsq == NULL and root->pDir == NULL)
+// 			return NULL;
+// 		else if (root->pEsq == NULL)
+// 		{
+// 			struct No *temp = root->pDir;
+// 			free(root);
+// 			return temp;
+// 		}
+// 		else if (root->pDir == NULL)
+// 		{
+// 			struct No *temp = root->pEsq;
+// 			free(root);
+// 			return temp;
+// 		}
+// 		return root;
+// 	}
+// }
+
+// void inorder(struct No *root)
+// {
+// 	if (root != NULL)
+// 	{
+// 		inorder(root->pEsq);
+// 		printf("%d  %d\n", root->Chave, root->FatBal);
+// 		inorder(root->pDir);
+// 	}
+// }
+
+// void printa(No *root)
+// {
+// 	if (root != nullptr)
+// 	{ /*inserir x como raiz da árvore*/
+// 		cout << root->Chave << endl;
+// 		printa(root->pEsq);
+// 		printa(root->pDir);
+// 	}
+// };
+
+// int getAltura(No *No)
+// {
+// 	if (No == nullptr)
+// 		return 0;
+// 	else
+// 	{
+// 		/* computa a altura de cada subArvore */
+// 		int lHeight = getAltura(No->pEsq);
+// 		int rHeight = getAltura(No->pDir);
+
+// 		/* usa a maior altura */
+// 		if (lHeight > rHeight)
+// 		{
+// 			return (lHeight + 1);
+// 		}
+// 		else
+// 		{
+// 			return (rHeight + 1);
+// 		}
+// 	}
+// }
+
+// void calculaFatBal(No *root)
+// {
+// 	if (root != nullptr)
+// 	{
+// 		calculaFatBal(root->pEsq);
+// 		calculaFatBal(root->pDir);
+
+// 		root->FatBal = (getAltura(root->pEsq) - getAltura(root->pDir));
+// 	}
+// }
+
---
+++ edalib.h
@@ -0,0 +1,137 @@
+#ifndef EDALIB_H
+#define EDALIB_H
+
+    #include <bits/stdc++.h>
+
+    void print_test(){
+        std::cout << "Olá mundooooooooo:" << std::endl;
+    }
+
+    void print_outro_test(){
+        std::cout << "Ainda tá funcionando!!!" << std::endl;
+    }
+
+    string replace(string values_string)
+    {
+
+        for (int i = 0; i < values_string.length(); i++)
+        {
+            if (values_string[i] == ',')
+            {
+                values_string[i] = ' ';
+            }
+        }
+        return values_string;
+    }
+
+    struct No
+    {
+        int Chave;
+        struct No *pEsq{}, *pDir{};
+        int FatBal;
+    };
+
+    void insere_node(int value, No *&root)
+    {
+        if (root == nullptr)
+        { /*inserir x como raiz da árvore*/
+            root = (No *)malloc(sizeof(No));
+            root->Chave = value;
+            root->pEsq = nullptr;
+            root->pDir = nullptr;
+        }
+        else
+        {
+            if (value < root->Chave)
+            {
+                insere_node(value, root->pEsq);
+            }
+            if (value > root->Chave)
+            {
+                insere_node(value, root->pDir);
+            }
+        }
+    };
+
+    struct No *liberaNo(struct No *root, int Chave)
+    {
+        if (root == NULL)
+            return root;
+        if (Chave < root->Chave)
+            root->pEsq = liberaNo(root->pEsq, Chave);
+        else if (Chave > root->Chave)
+            root->pDir = liberaNo(root->pDir, Chave);
+        else
+        {
+            if (root->pEsq == NULL and root->pDir == NULL)
+                return NULL;
+            else if (root->pEsq == NULL)
+            {
+                struct No *temp = root->pDir;
+                free(root);
+                return temp;
+            }
+            else if (root->pDir == NULL)
+            {
+                struct No *temp = root->pEsq;
+                free(root);
+                return temp;
+            }
+            return root;
+        }
+    }
+
+    void inorder(struct No *root)
+    {
+        if (root != NULL)
+        {
+            inorder(root->pEsq);
+            printf("%d  %d\n", root->Chave, root->FatBal);
+            inorder(root->pDir);
+        }
+    }
+
+    void printa(No *root)
+    {
+        if (root != nullptr)
+        { /*inserir x como raiz da árvore*/
+            std::cout << root->Chave << std::endl;
+            printa(root->pEsq);
+            printa(root->pDir);
+        }
+    };
+
+    int getAltura(No *No)
+    {
+        if (No == nullptr)
+            return 0;
+        else
+        {
+            /* computa a altura de cada subArvore */
+            int lHeight = getAltura(No->pEsq);
+            int rHeight = getAltura(No->pDir);
+
+            /* usa a maior altura */
+            if (lHeight > rHeight)
+            {
+                return (lHeight + 1);
+            }
+            else
+            {
+                return (rHeight + 1);
+            }
+        }
+    }
+
+    void calculaFatBal(No *root)
+    {
+        if (root != nullptr)
+        {
+            calculaFatBal(root->pEsq);
+            calculaFatBal(root->pDir);
+
+            root->FatBal = (getAltura(root->pEsq) - getAltura(root->pDir));
+        }
+    }
+
+#endif
